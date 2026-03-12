1b16c7b9ab3bfc609d359b9abaa14a4727617f40
EniV90 <eni_v90@yahoo.com>
2026-03-12 05:55:14 +0100

    chore: setup project structure and configuration

---
+++ to-do/core/src/enums.rs
@@ -0,0 +1,5 @@
+#[derive(Debug, PartialEq)]
+pub enum TaskStatus {
+    DONE,
+    PENDING,
+}
---
+++ to-do/core/src/main.rs
@@ -0,0 +1,30 @@
+use clap::{Parser, parser};
+use std::env;
+
+mod enums;
+mod structs;
+mod api;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+
+struct Args {
+    /// first name of user
+    #[arg(short, long)]
+    first_name: String,
+
+    /// last name of user
+    #[arg(short, long)]
+    last_name: String,
+
+    /// age of user
+    #[arg(short, long, default_value_t = 1)]
+    age: u8,
+}
+fn main() {
+    let args = Args::parse();
+
+    println!("{:?}", args.first_name);
+    println!("{:?}", args.last_name);
+    println!("{:?}", args.age);
+}
